56db64ecab7c3d2bd36ff2331766b3a762945c6a
Yuze <xzhdavid@hotmail.com>
2022-06-13 21:39:31 +0200

    index

--- static/script/scritp.js
+++ static/script/scritp.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function() {
     const navbar = document.querySelector("nav");
-    navbar.classList.toggle("sticky", window.scrollY > 0);
+    navbar.classList.toggle("sticky-top", window.scrollY > 0);
 });
