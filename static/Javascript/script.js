4cc81779987f5a7fe4fbad9209e9c07dd2ec2e8e
Yuze <xzhdavid@hotmail.com>
2023-02-23 15:02:59 +0100

    index

--- static/Javascript/script.js
+++ static/Javascript/script.js
@@ -4,5 +4,5 @@ let now = new Date().getTime();
 let myBirthday = new Date("2008-10-09T05:00:00").getTime();
 let curr_age = Math.floor((now-myBirthday)/1000/60/60/24/365.25)
 
-let age = document.querySelector(".js_age");
+let age = document.querySelector("p.desc");
 age.innerHTML = "I'm Yuze Xing and " + curr_age + " years old."
