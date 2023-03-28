b20c31f868571980bd58952108ebd4ad76adfe9b
Yuze <xzhdavid@hotmail.com>
2023-03-28 10:41:11 +0200

    ...

--- static/Javascript/script.js
+++ static/Javascript/script.js
@@ -1,8 +1,26 @@
-// my age 
+let btn = document.querySelector("button.mode");
+let content = btn.innerHTML;
+let link = document.querySelectorAll("link")[0];
+let style1 = 'static/scss/style1.css';
+let style2 = 'static/scss/style2.css';
 
-let now = new Date().getTime();
-let myBirthday = new Date("2008-10-09T05:00:00").getTime();
-let curr_age = Math.floor((now-myBirthday)/1000/60/60/24/365.25)
+btn.onclick = () => {
+  let currentStyle = link.getAttribute("href");
+  if (currentStyle === style1) {
+    link.setAttribute("href", style2);
+    let icon = document.createElement("i");
+    icon.classList.add("fa-solid", "fa-sun");
+    btn.innerHTML = " Light mode";
+    btn.insertBefore(icon, btn.firstChild);
+  } else {
+    link.setAttribute("href", style1);
+    let icon = document.createElement("i");
+    icon.classList.add("fa-solid", "fa-moon");
+    btn.innerHTML = " Dark mode";
+    btn.insertBefore(icon, btn.firstChild);
+  }
+}
 
-let age = document.querySelector("p.desc");
-age.innerHTML = "I'm Yuze Xing and " + curr_age + " years old."
+let navHeight = document.querySelector(".navbar").offsetHeight;
+document.querySelector(".home").style.paddingTop = navHeight + "px";
+
